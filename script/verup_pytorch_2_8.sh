2a43489c3f89008f29861e26f3ca1247e58db558
mayu4591 <concorde2591@gmail.com>
2026-03-09 01:54:42 +0900

    Automates xformers build with CUDA 12.8 installation
    Replaces static xformers wheel installation with dynamic build
    process and ensures CUDA 12.8 is present. Improves portability
    and compatibility across environments by automating dependency
    setup and build steps.

--- script/verup_pytorch_2_8.sh
+++ script/verup_pytorch_2_8.sh
@@ -6,8 +6,34 @@ pip install -U --pre torch==2.9.1 torchvision torchaudio --index-url https://dow
 # ファイルのディレクトリを取得
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-# xformersのインストール
-pip install $SCRIPT_DIR/package/xformers-0.0.31+eb3ea378.d20250621-cp39-abi3-linux_x86_64.whl
+# xformersのインストール(環境によって変わるので都度ビルド)
+## CUDA 12.8がない場合、インストールする
+if [ ! -d "/usr/local/cuda-12.8" ]; then
+    echo "CUDA 12.8 is not installed. Installing CUDA 12.8..."
+    rm -f /etc/apt/sources.list.d/cuda.list
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+    dpkg -i cuda-keyring_1.1-1_all.deb
+    apt-get update
+    apt-get -y install cuda-toolkit-12-8
+    echo "CUDA 12.8 has been installed."
+fi
+
+sudo apt update
+sudo apt install python3-dev build-essential cmake
+git clone https://github.com/facebookresearch/xformers.git
+cd xformers
+git submodule update --init --recursive
+
+export CUDA_HOME="/usr/local/cuda-12.8"
+export PATH="$CUDA_HOME/bin:$PATH"
+export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$LD_LIBRARY_PATH"
+export TORCH_CUDA_ARCH_LIST="12.0"
+
+pip install -r requirements.txt
+pip install ninja packaging wheel
+pip install .
+cd ..
+
 
 # pythonのバージョンを確認
 echo "=========================results========================="
